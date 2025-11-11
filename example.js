74c76aa95853eb3d3e48ffcd419dc6afbc0832fe
Chit Hmue Than Thar <juliawilson757@gmail.com>
2025-11-11 22:31:16 +0630

    Add a auth function example

--- example.js
+++ example.js
@@ -1,1 +1,17 @@
 print("This is in feature/day2 branch")
+// example code
+function isAuthenticated(req, _res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+    if (!token) {
+        throw new errors_1.UnauthorizedError('Access token is required');
+    }
+    try {
+        const decoded = (0, jwt_1.verifyAccessToken)(token);
+        req.user = decoded;
+        next();
+    }
+    catch (error) {
+        throw new errors_1.UnauthorizedError('Invalid or expired token');
+    }
+}
